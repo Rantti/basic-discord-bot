ee7629cda9ca07b07b30acf59f9f0aa4543abe5c
Rantti <antti.o.eloranta@gmail.com>
2020-10-11 19:04:14 +0300

    Add template file

---
+++ commands/template.js
@@ -0,0 +1,7 @@
+module.exports = {
+    command: 'template',
+    description: 'Template command', // Description, used in "list" command.
+    callBack: function(message, args) {
+        // Your command logic here
+    },
+};
